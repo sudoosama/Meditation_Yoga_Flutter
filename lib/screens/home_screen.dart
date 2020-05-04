cc24fcaa099708b2718970b2242ed9a8987de711
OsamaxD1 <hassanosama16b@gmail.com>
2020-05-04 23:23:09 +0500

    homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:bookreaderflutter/colorsfile.dart';
+import 'package:bookreaderflutter/widget/book_rating.dart';
+import 'package:bookreaderflutter/widget/two_sided_rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -21,19 +24,154 @@ class HomeScreen extends StatelessWidget {
               height: size.height * .1,
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24,),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+              ),
               child: RichText(
-                  text: TextSpan(
-                      style: Theme.of(context).textTheme.display1,
-                      children: [
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.display1,
+                  children: [
                     TextSpan(text: "What are you \nreading "),
-                    TextSpan(text: "today?",style: TextStyle(
-                      fontWeight: FontWeight.bold
-                    ))
-                      ])),
-            )
+                    TextSpan(
+                        text: "today?",
+                        style: TextStyle(fontWeight: FontWeight.bold))
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ReadingListCard(
+              image: "images/book-1.png",
+              title: "Crushing & Influence",
+              auth: "Grey Venchuk",
+              rating: 4.9,
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ReadingListCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String auth;
+  final double rating;
+  final Function pressDetails;
+  final Function pressRead;
+  const ReadingListCard({
+    Key key,
+    this.image,
+    this.title,
+    this.auth,
+    this.rating,
+    this.pressDetails,
+    this.pressRead,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 24, bottom: 40),
+      height: 245,
+      width: 202,
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 10,
+            right: 0,
+            child: Container(
+              height: 221,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(29),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 33,
+                    color: kShadowColor,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Image.asset(
+            image,
+            width: 150,
+          ),
+          Positioned(
+            top: 35,
+            right: 10,
+            child: Column(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  onPressed: () {},
+                ),
+                BookRating(score: rating),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 160,
+            child: Container(
+              height: 85,
+              width: 202,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 24),
+                    child: RichText(
+                      maxLines: 2,
+                      text: TextSpan(
+                        style: TextStyle(color: kBlackColor),
+                        children: [
+                          TextSpan(
+                            text: "$title\n",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: auth,
+                            style: TextStyle(
+                              color: kLightBlackColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: pressDetails,
+                        child: Container(
+                          width: 101,
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.center,
+                          child: Text("Details"),
+                        ),
+                      ),
+                      Expanded(
+                        child: TwoSideRoundedButton(
+                          text: "Read",
+                          press: pressRead,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
